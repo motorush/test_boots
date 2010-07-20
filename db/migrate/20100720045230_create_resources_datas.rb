3f9bdc8a301b76a60c0d35e48b6afc87b273d210
JeBlanc <jblanco@clinic-it.com>
2010-07-20 16:03:51 +0800

    Added resources_field_id to ResourcesData

--- db/migrate/20100720045230_create_resources_datas.rb
+++ db/migrate/20100720045230_create_resources_datas.rb
@@ -1,6 +1,7 @@
 class CreateResourcesDatas < ActiveRecord::Migration
   def self.up
     create_table :resources_datas do |t|
+      t.references :resources_field
       t.string :value
 
       t.timestamps
